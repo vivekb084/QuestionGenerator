d509cf3fa607db99de52779a3033f77efecacf6b
vivek.bindal <vivek.bindal@ebizontek.com>
2020-08-09 14:29:32 +0530

    shuffle addded

--- controllers/calculation.js
+++ controllers/calculation.js
@@ -28,13 +28,14 @@ const GenerateQuestion = async(req,res)=>{
         for(let i=0;i<totalquestions;i++){
             let Minuend = getNumber(minuedDigitCount);
             let Subtrahend = getNumber(SubtrahendDigitCount);
+
             let subtractedValue = Minuend-Subtrahend;
-            let Options = [];
-            for(let i=0;i<3;i++){
-                Options.push(getNumber(minuedDigitCount))
-            }
-            let randomIndex = Math.floor(Math.random() * (3 - 0 + 1))
-            Options.splice(randomIndex,0,subtractedValue)
+            let Options = [subtractedValue-1,subtractedValue+1,subtractedValue-getNumber(2),subtractedValue];
+
+            Options = await convertToPositive(Options); //Convert All elements to positive
+
+            Options = await shuffle(Options) //Shuffle the Options of array
+
             let Question ={
                 'Question':i+1,
                 Minuend,
@@ -46,6 +47,7 @@ const GenerateQuestion = async(req,res)=>{
         }
         response.sendsuccessData(res,"Questions Generated Successfully",QuestionArray)
     } catch (error) {
+        console.error("Error ",error)
         response.sendErrorCustomMessage(res, "Internal Server Error", "false");
     }
 }
@@ -57,6 +59,38 @@ const getNumber = (digits)=>{
         .random() * (MaxNum - MinNum + 1)) + MinNum;
     return randomNumber;
 }
+
+const shuffle = (array)=>{
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+}
+
+const convertToPositive = (array)=>{
+    let newArray=[];
+    for(let i=0;i<array.length;i++){
+        if(array[i]<0){
+            newArray.push(-1*array[i]);
+        }
+        else{
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+}  
 
 
 module.exports ={
